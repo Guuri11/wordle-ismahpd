086b981ea49bbf5373d827c9b6d936521823d939
Sergio Gurillo Corral <48799796+Guuri11@users.noreply.github.com>
2022-03-01 01:41:07 +0100

    Update wordle-ismahpd.py

--- wordle-ismahpd.py
+++ wordle-ismahpd.py
@@ -1,40 +1,56 @@
 import random
+import json
 
-WORDS = ['adios', 'reino', 'balon', 'palas', 'palos']
-ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# remove unique values
+def checkio(data):
+    for index in range(len(data) - 1, -1, -1):
+        if data.count(data[index]) == 1:
+            del data[index]
+    return data
+
+
+# Opening JSON file
+f = open('words.json')
+ 
+# returns JSON object as
+# a dictionary
+WORDS = json.load(f)
+
 word = random.choice(WORDS)
-playerInput = ''.join(random.choices(ALPHABET, k=5))
-playerCheck = [0,0,0,0,0] # 0: false, 1: true, 2: not in correct place
+playerInput = 'aueio'
+playerCheck = [0,0,0,0,0] # 0: false, 1: true, 2: not in correct place
 tries = 6
 attempts = 0
-while word != playerInput and tries > 0:
+while word.lower() != playerInput.lower() and tries > 0:
     attempts += 1
+    if playerInput in WORDS:
+        WORDS.remove(playerInput)
     for piIndex in range(0, len(playerInput)):
         if playerInput[piIndex] == word[piIndex]:
             playerCheck[piIndex] = 1
         elif playerInput[piIndex] in word:
             playerCheck[piIndex] = 2
-        elif playerInput[piIndex] not in word and playerInput[piIndex] in ALPHABET:
-            ALPHABET.remove(playerInput[piIndex])
-    for pcIndex in range(0, len(playerCheck)):
-        replacements = []
-        if playerCheck[pcIndex] == 0 or playerCheck[pcIndex] == 2:
-            s = list(playerInput)
-            s[pcIndex] = ''.join(random.choices(ALPHABET, k=1))
-            playerInput = "".join(s)
-        if playerCheck[pcIndex] == 2:
-            replacements.append(pcIndex)
     
-    # Replace playerChecks' 2s
-    if len(replacements) > 1:
-        for rpIndex in range(0, len(replacements)-1):
-            s = list(playerInput)
-            aux = s[replacements[rpIndex+1]]
-            s[replacements[rpIndex+1]] = s[replacements[rpIndex]]
-            s[replacements[rpIndex]] = aux
-            playerInput = "".join(s)
-    print(ALPHABET)
-    print(playerInput)
-    print(playerCheck)
-    print(attempts)
+    for pcIndex in range(0, len(playerCheck)):
+        if playerCheck[pcIndex] == 1:
+            possibleWords = []
+            for w in WORDS:
+                if playerInput[pcIndex] == w[pcIndex]:
+                    possibleWords.append(w)
+                    WORDS = possibleWords
+        elif playerCheck[pcIndex] == 2:
+            possibleWords = []
+            for w in WORDS:
+                if playerInput[pcIndex] in w:
+                    possibleWords.append(w)
+
+    possibleWords = checkio(possibleWords)
+    if len(possibleWords) > 1:
+        print(possibleWords)
+        playerInput = random.choice(possibleWords)
+        print(playerInput)
+    tries -= 1
+
+print("\n")
+print(word)
+print(attempts)
